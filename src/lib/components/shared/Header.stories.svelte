7d0128b8f93eee39b86365ea5eedd33aabff8117
Luke Gareth Ribchester <luke@luke.gr>
2022-05-03 23:28:30 +0200

    feat: create the `Header` component
    Signed-off-by: Luke Gareth Ribchester <luke@luke.gr>

---
+++ src/lib/components/shared/Header.stories.svelte
@@ -0,0 +1,17 @@
+<script>
+    import { Meta, Story, Template } from "@storybook/addon-svelte-csf";
+    import Header from "./Header.svelte";
+</script>
+
+<Meta title="Shared/Header"
+      component={Header}
+      argTypes={{}}
+/>
+
+<Template let:args>
+    <Header {...args}/>
+</Template>
+
+<Story name="Primary"
+       args={{}}
+/>
---
+++ src/lib/components/shared/Header.svelte
@@ -0,0 +1,19 @@
+<h1><a href="/">Tessa Greiner</a></h1>
+
+<style>
+    h1 {
+        font-family: 'ui-serif', 'Times New Roman', Times, serif;
+        font-size: 1.75rem;
+        font-weight: 600;
+        color: rgb(65, 65, 65);
+        text-transform: uppercase;
+        text-align: center;
+        margin: 0;
+        padding: 0;
+    }
+
+    h1 a {
+        color: inherit;
+        text-decoration: inherit;
+    }
+</style>
