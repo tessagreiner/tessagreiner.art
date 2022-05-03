5e58d3c2bf56ae0b92ca8f32123ba725be93b5ab
Luke Gareth Ribchester <luke@luke.gr>
2022-05-03 23:29:08 +0200

    feat: create the `Navigation` component
    Signed-off-by: Luke Gareth Ribchester <luke@luke.gr>

---
+++ src/lib/components/shared/Navigation.stories.svelte
@@ -0,0 +1,17 @@
+<script>
+    import { Meta, Story, Template } from "@storybook/addon-svelte-csf";
+    import Navigation from "./Navigation.svelte";
+</script>
+
+<Meta title="Shared/Navigation"
+      component={Navigation}
+      argTypes={{}}
+/>
+
+<Template let:args>
+    <Navigation {...args}/>
+</Template>
+
+<Story name="Primary"
+       args={{}}
+/>
---
+++ src/lib/components/shared/Navigation.svelte
@@ -0,0 +1,44 @@
+<nav>
+    <ul>
+        <li><a href="./art">Art</a></li>
+        <li><a href="./bio">Bio</a></li>
+        <li><a href="./contact">Contact</a></li>
+    </ul>
+</nav>
+
+<style>
+    nav {
+    }
+
+    ul {
+        display: flex;
+        flex-direction: row;
+        flex-wrap: wrap;
+        justify-content: center;
+        align-items: center;
+        align-content: center;
+        gap: 1em;
+        margin: 0;
+        padding: 0;
+        list-style-type: none;
+    }
+
+    ul li {
+        display: inline-block;
+    }
+
+    ul li a {
+        font-family: 'ui-sans-serif', 'Helvetica Neue', sans-serif;
+        font-size: 1.15rem;
+        font-weight: 300;
+        color: rgb(65, 65, 65);
+        text-transform: uppercase;
+        text-decoration: none;
+    }
+
+    @media (min-width: 600px) {
+        ul {
+            gap: 2em;
+        }
+    }
+</style>
