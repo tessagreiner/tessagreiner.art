24df0eaba50bbcc26ad6f5765fd888572acf100b
Luke Gareth Ribchester <luke@luke.gr>
2022-05-03 23:32:07 +0200

    feat: create the `Grid` component
    Signed-off-by: Luke Gareth Ribchester <luke@luke.gr>

---
+++ src/lib/components/art/Grid.stories.svelte
@@ -0,0 +1,17 @@
+<script>
+    import { Meta, Story, Template } from "@storybook/addon-svelte-csf";
+    import Grid from "./Grid.svelte";
+</script>
+
+<Meta title="Art/Grid"
+      component={Grid}
+      argTypes={{}}
+/>
+
+<Template let:args>
+    <Grid {...args}/>
+</Template>
+
+<Story name="Primary"
+       args={{}}
+/>
---
+++ src/lib/components/art/Grid.svelte
@@ -0,0 +1,27 @@
+<div class="grid">
+    <slot></slot>
+</div>
+
+<style>
+    .grid {
+        display: grid;
+        grid-template-columns: repeat(1, auto);
+        justify-content: center;
+        align-items: stretch;
+        align-content: center;
+        grid-gap: 4em;
+        padding: 4rem;
+    }
+
+    @media (min-width: 36rem) {
+        .grid {
+            grid-template-columns: repeat(2, auto);
+        }
+    }
+
+    @media (min-width: 52rem) {
+        .grid {
+            grid-template-columns: repeat(3, auto);
+        }
+    }
+</style>
