b773f2385643ca7f824a7a862d816f7405d03f1b
Luke Gareth Ribchester <luke@luke.gr>
2022-05-03 23:29:50 +0200

    feat: create the `AppBar` component
    Signed-off-by: Luke Gareth Ribchester <luke@luke.gr>

---
+++ src/lib/components/shared/AppBar.stories.svelte
@@ -0,0 +1,17 @@
+<script>
+    import { Meta, Story, Template } from "@storybook/addon-svelte-csf";
+    import AppBar from "./AppBar.svelte";
+</script>
+
+<Meta title="Shared/AppBar"
+      component={AppBar}
+      argTypes={{}}
+/>
+
+<Template let:args>
+    <AppBar {...args}/>
+</Template>
+
+<Story name="Primary"
+       args={{}}
+/>
---
+++ src/lib/components/shared/AppBar.svelte
@@ -0,0 +1,37 @@
+<script lang="ts">
+  import Header from './Header.svelte';
+  import Navigation from './Navigation.svelte';
+
+  export let raised = true;
+</script>
+
+<div class="app-bar {raised === true ? 'raised' : ''}">
+    <Header/>
+
+    <Navigation/>
+</div>
+
+<style>
+    .app-bar {
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+        align-content: center;
+        gap: 1em;
+        padding: 2rem;
+    }
+
+    .raised {
+        box-shadow: 0 2px 4px -3px;
+    }
+
+    @media (min-width: 600px) {
+        .app-bar {
+            flex-direction: row;
+            justify-content: space-between;
+            gap: 0;
+            padding: 2rem 2rem;
+        }
+    }
+</style>
