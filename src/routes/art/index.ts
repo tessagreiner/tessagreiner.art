840ae0192f450bcb19a36ea9fbc0ab37c62e2f9e
Luke Gareth Ribchester <luke@luke.gr>
2022-09-20 15:16:49 +0100

    fix: rename `get` to `GET`
    Signed-off-by: Luke Gareth Ribchester <luke@luke.gr>

--- src/routes/art/index.ts
+++ src/routes/art/index.ts
@@ -1,4 +1,4 @@
-export async function get() {
+export async function GET() {
   const response = await fetch('http://localhost:3000/artwork/artwork.json');
   const artwork = await response.json();
 
